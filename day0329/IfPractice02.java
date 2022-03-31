7a848044a06d1427ad944b7068b438bc46ec0afd
Doyeon Kim <60147434+doyeon111@users.noreply.github.com>
2022-03-31 21:10:55 +0900

    가장 큰 수 구하기

--- day0329/IfPractice02.java
+++ day0329/IfPractice02.java
@@ -8,30 +8,30 @@ public class IfPractice02 {
 		Scanner sc = new Scanner(System.in);
 		int a, b, c, max;
 		
-		System.out.print("ù ��° ���ڸ� �Է��ϼ���: ");
+		System.out.print("첫 번째 숫자를 입력하세요: ");
 		a = sc.nextInt();
-		System.out.print("�� ��° ���ڸ� �Է��ϼ���: ");
+		System.out.print("두 번째 숫자를 입력하세요: ");
 		b = sc.nextInt();
-		System.out.print("�� ��° ���ڸ� �Է��ϼ���: ");
+		System.out.print("세 번째 숫자를 입력하세요: ");
 		c = sc.nextInt();
 		
 		
-		if (a > b) {
-			if (a > c) {
-				max = a;
+		if (a > b) { //a와b를 비교하여 a가 더 클 경우
+			if (a > c) { //a와 c를 비교하여 a가 클 경우
+				max = a; //a가 가장 큰 수이다.
 			} else {
-				max = c;
+				max = c; //a와 c를 비교하여 c가 클경우는 c가 max
 			}
-		} else {
-			if (b > c) {
-				max = b;
-			} else {
-				max = c;
+		} else { //a와 b를 비교하여 b가 더 클 경우
+			if (b > c) { //b와 c를 비교하여 b가 더 클 경우
+				max = b; //b가 최대값
+			} else { //b와 c를 비교하여 c가 더 클 경우
+				max = c; // c의 값이 max
 			}
 
 		}
 		
-		System.out.println("���� ū ���� " + max);
+		System.out.println("가장 큰 수는 " + max);
 
 	}
 }
