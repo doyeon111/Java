9fd45fc415640eaa123206c786d0258b7b9da7dd
Doyeon Kim <60147434+doyeon111@users.noreply.github.com>
2022-04-01 22:35:50 +0900

    구구단 2-9단까지 출력

--- day0330/GugudanPractice.java
+++ day0330/GugudanPractice.java
@@ -4,9 +4,9 @@ public class GugudanPractice {
 
 	public static void main(String[] args) {
 
-		for(int i=2; i<=9; i++) {
-			System.out.println(i + "��");
-			for(int j=1; j<=9; j++) {
+		for(int i=2; i<=9; i++) { //2단부터 9단까지 1씩 증가하며 실행
+			System.out.println(i + "단"); 
+			for(int j=1; j<=9; j++) { // 1부터 9까지 실행
 				System.out.println(i + "*" + j + "=" + (i*j));
 			}
 			System.out.println("---------------------------");
