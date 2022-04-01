c54f9ad7d342ea09e1f44f74aa92756cc460de0a
Doyeon Kim <60147434+doyeon111@users.noreply.github.com>
2022-04-01 22:34:41 +0900

    For문 - hello 출력하기

--- day0330/ForTest01.java
+++ day0330/ForTest01.java
@@ -5,7 +5,7 @@ public class ForTest01 {
 	public static void main(String[] args) {
 		int i;
 		
-		for(i = 1; i <= 5; i++) {
+		for(i = 1; i <= 5; i++) { //i가 1부터 5번째까지 hello 출력
 			System.out.println("hello");
 		}
 
