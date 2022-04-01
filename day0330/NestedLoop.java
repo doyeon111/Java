83b3def3ba574dbfe887309c3ec92be290c95774
Doyeon Kim <60147434+doyeon111@users.noreply.github.com>
2022-04-01 22:38:56 +0900

    루프

--- day0330/NestedLoop.java
+++ day0330/NestedLoop.java
@@ -3,9 +3,9 @@ package day0330;
 public class NestedLoop {
 
 	public static void main(String[] args) {
-		for(int i=1; i<=3; i++) {
+		for(int i=1; i<=3; i++) { //i를 3번째까지 출력
 			System.out.println(i);
-			for(int j=1; j<=4; j++) {
+			for(int j=1; j<=4; j++) { //hello를 4번 반복출력
 				System.out.println("hello");
 			}
 			System.out.println("---------------------------");
