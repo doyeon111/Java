5be70222b364bd4c4832c472c63580284ce51c8f
Doyeon Kim <60147434+doyeon111@users.noreply.github.com>
2022-04-10 15:32:52 +0900

    모든 예외처리와 자식클래스의 예외처리

--- day0408/exam04/DivTest04.java
+++ day0408/exam04/DivTest04.java
@@ -2,9 +2,9 @@ package exam04;
 
 import java.util.Scanner;
 
-//ÇÁ·Î±×·¥ ½ÇÇà ½Ã µÎ °³ÀÇ Á¤¼ö¸¦ Àü´Ş¹Ş¾Æ ³ª´©±â ÇÑ °á°ú¸¦ Ãâ·Â
+//í”„ë¡œê·¸ë¨ ì‹¤í–‰ ì‹œ ë‘ ê°œì˜ ì •ìˆ˜ë¥¼ ì „ë‹¬ë°›ì•„ ë‚˜ëˆ„ê¸° í•œ ê²°ê³¼ë¥¼ ì¶œë ¥
 //java DivTest 4 2 
-//¹ß»ıÇÒ ¼ö ÀÖ´Â ´Ù¾çÇÑ ¿¹¿Ü¿¡ ´ëÇÑ Ã³¸®¸¦ ±¸Çö
+//ë°œìƒí•  ìˆ˜ ìˆëŠ” ë‹¤ì–‘í•œ ì˜ˆì™¸ì— ëŒ€í•œ ì²˜ë¦¬ë¥¼ êµ¬í˜„
 public class DivTest04 {
 
 	public static void main(String[] args) {
@@ -14,13 +14,14 @@ public class DivTest04 {
 			a = Integer.parseInt(args[0]);
 			b = Integer.parseInt(args[1]);
 			div = a / b;
-			System.out.println("³ª´©±â °á°ú: " + div);
-		} catch (ArithmeticException e) { // catch¹®À» ½áÁÙ ¶§´Â ÀÚ½ÄÅ¬·¡½ººÎÅÍ ¿Í¾ß ÇÑ´Ù.
+			System.out.println("ë‚˜ëˆ„ê¸° ê²°ê³¼: " + div);
+		} catch (ArithmeticException e) { // catchë¬¸ì„ ì¨ì¤„ ë•ŒëŠ” ìì‹í´ë˜ìŠ¤ë¶€í„° ì™€ì•¼ í•œë‹¤.
 
-			System.out.println("0À¸·Î´Â ³ª´­ ¼ö ¾ø¾î¿ä!");
-		} catch (Exception e) { // ¸ğµç ¿¹¿ÜÃ³¸®
-			System.out.println("³ª´©±â ÇÒ µÎ °³ÀÇ Á¤¼ö¸¦ Àü´ŞÇØ ÁÖ¼¼¿ä.");
+			System.out.println("0ìœ¼ë¡œëŠ” ë‚˜ëˆŒ ìˆ˜ ì—†ì–´ìš”!");
+		} catch (Exception e) { // ëª¨ë“  ì˜ˆì™¸ì²˜ë¦¬
+			System.out.println("ë‚˜ëˆ„ê¸° í•  ë‘ ê°œì˜ ì •ìˆ˜ë¥¼ ì „ë‹¬í•´ ì£¼ì„¸ìš”.");
 		}
 	}
 
 }
+ 
