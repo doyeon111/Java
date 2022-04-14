148dc5a027a928324b834316bbe2086d0a0039b7
Doyeon Kim <60147434+doyeon111@users.noreply.github.com>
2022-04-14 22:32:48 +0900

    TCP통신 클라이언트

--- day0413/exam03/TCPClient.java
+++ day0413/exam03/TCPClient.java
@@ -8,30 +8,30 @@ public class TCPClient {
 
 	public static void main(String[] args) {
 		try {
-			// 3. ¼­¹ö¿¡°Ô Åë½ÅÀ» ¿äÃ»ÇÑ´Ù.
-			Socket socket = new Socket("192.168.0.2", 9000); // ipÁÖ¼Ò¿Í Æ÷Æ®¹øÈ£ 9000¹ø (server¿¡¼­ ¼³Á¤ÇØÁÜ) //cmd¿¡¼­ È®ÀÎÇÑ ¾ÆÀÌÇÇÁÖ¼Ò¸¦
-																// Àû¾îÁØ´Ù.
+			// 3. ì„œë²„ì—ê²Œ í†µì‹ ì„ ìš”ì²­í•œë‹¤.
+			Socket socket = new Socket("192.168.0.2", 9000); // ipì£¼ì†Œì™€ í¬íŠ¸ë²ˆí˜¸ 9000ë²ˆ (serverì—ì„œ ì„¤ì •í•´ì¤Œ) //cmdì—ì„œ í™•ì¸í•œ ì•„ì´í”¼ì£¼ì†Œë¥¼
+																// ì ì–´ì¤€ë‹¤.
 
-			// 5. µ¥ÀÌÅÍ¸¦ ÁÖ°í¹ŞÀ» ½ºÆ®¸²À» »ı¼º
+			// 5. ë°ì´í„°ë¥¼ ì£¼ê³ ë°›ì„ ìŠ¤íŠ¸ë¦¼ì„ ìƒì„±
 			InputStream is = socket.getInputStream();
 			OutputStream os = socket.getOutputStream();
 			
-			// 6. µ¥ÀÌÅÍ¸¦ ÁÖ°í¹Ş´Â´Ù.
+			// 6. ë°ì´í„°ë¥¼ ì£¼ê³ ë°›ëŠ”ë‹¤.
 			for(int i=1; i<=10; i++) {
-				int n = is.read(); //¼ö½ÅµÈ µ¥ÀÌÅÍ¸¦ ÀĞ¾î¿È.
-				System.out.println("¼­¹ö·Î ºÎÅÍ ¼ö½ÅµÈ µ¥ÀÌÅÍ: " +n);
+				int n = is.read(); //ìˆ˜ì‹ ëœ ë°ì´í„°ë¥¼ ì½ì–´ì˜´.
+				System.out.println("ì„œë²„ë¡œ ë¶€í„° ìˆ˜ì‹ ëœ ë°ì´í„°: " +n);
 				
 				Thread.sleep(200);
 				
 			}
 			
-			// 7. »ç¿ëÇß´ø ÀÚ¿øÀ» ´İ¾ÆÁØ´Ù.
+			// 7. ì‚¬ìš©í–ˆë˜ ìì›ì„ ë‹«ì•„ì¤€ë‹¤. 
 			is.close();
 			os.close();
 			socket.close();	
 
 		} catch (Exception e) {
-			System.out.println("¿¹¿Ü¹ß»ı: " + e.getMessage());
+			System.out.println("ì˜ˆì™¸ë°œìƒ: " + e.getMessage());
 		}
 
 	}
