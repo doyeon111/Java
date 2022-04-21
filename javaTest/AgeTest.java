0416f6d269104cb01060719027ade6e3a7da1b4e
Doyeon Kim <60147434+doyeon111@users.noreply.github.com>
2022-04-21 21:05:42 +0900

    javaTest

---
+++ javaTest/AgeTest.java
@@ -0,0 +1,24 @@
+package day0328;
+
+import java.util.Scanner;
+
+public class AgeTest {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int age;
+		
+		System.out.print("나이를 입력하세요: ");
+		age = sc.nextInt();
+		
+		if (age >= 10 && age <= 19) {
+			System.out.println("10대 입니다.");
+		} else {
+			System.out.println("10대가 아닙니다.");
+		}
+		
+
+	}
+
+}
---
+++ javaTest/OrdersDAO.java
@@ -0,0 +1,60 @@
+package com.sist.dao;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import com.sist.vo.OrdersVO;
+
+//고객번호를 입력받아 그 고객이 주문한 주문번호, 고객번호, 도서번호, 구매금액, 구매일을 출력하는 프로그램을 작성
+
+//데이터베이스에 접근하여 Orders 테이블의 자료를 추가하거나 수정하거나 검색하거나 삭제를 위한 메소드를 갖고있는 클래스를 만든다.
+public class OrdersDAO {
+	// 고객 아이디를 매개변수로 전달받아 해당 고객의 주문내역을 검색하여 ArrayList로 반환하는 메소드를 정의
+	public ArrayList<OrdersVO> listOrder(int custid) {
+		ArrayList<OrdersVO> list = new ArrayList<OrdersVO>();
+
+		String sql = "select * from orders where custid = " + custid;
+
+		// 데이터베이스 명령어를 실행하는데 까지 코드를 완성
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@192.168.35.171:1521:XE", "c##sist",
+					"sist");
+
+			Statement stmt = conn.createStatement();
+
+			ResultSet rs = stmt.executeQuery(sql);
+
+			// 검색한 결과만큼 반복 실행
+			while (rs.next()) {
+				// 현재 커서가 바라보고 있는 곳에 데이터를 뽑아온다.
+				int orderid = rs.getInt(1);
+				int cid = rs.getInt(2);
+				int bookid = rs.getInt(3);
+				int saleprice = rs.getInt(4);
+				Date orderdate = rs.getDate(5);
+
+				// 현재 커서가 바라보고있는 곳에 데이터를 갖고 VO를 생성
+				OrdersVO vo = new OrdersVO(orderid, custid, bookid, saleprice, orderdate);
+
+				// 생성한 VO를 리스트에 담는다.
+				list.add(vo);
+			}
+
+			// 사용했던 자원들을 닫아준다.
+			rs.close();
+			stmt.close();
+			conn.close();
+
+		} catch (Exception e) {
+			System.out.println("예외발생: " + e.getMessage());
+		}
+
+		return list;
+	}
+}
