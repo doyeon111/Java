43f7141294cba70fea01bf67059ef954375e05ad
Doyeon Kim <60147434+doyeon111@users.noreply.github.com>
2022-05-11 23:34:02 +0900

    0511

---
+++ oracleTest/day0511/dao/SurveyDAO.java
@@ -0,0 +1,45 @@
+package com.sist.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import com.sist.vo.SurveyVO;
+
+public class SurveyDAO {
+	private String driver = "oracle.jdbc.driver.OracleDriver";
+	private String url = "jdbc:oracle:thin:@192.168.35.55:1521:XE";
+	private String user ="c##sist";
+	private String pwd = "sist";
+	
+	public int insertSurvey(SurveyVO s) {
+		int re = -1;
+		String sql = "insert into survey values((select nvl(max(no), 0) + 1 from survey),?,?,?,?,?)";
+		
+		try {
+			Class.forName(driver);
+			Connection conn = DriverManager.getConnection(url, user, pwd);
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, s.getGrade());
+			pstmt.setString(2, s.getGender());
+			pstmt.setString(3, s.getInterests());
+			pstmt.setString(4, s.getCourse());
+			pstmt.setString(5, s.getLeave());
+			
+			re = pstmt.executeUpdate();
+			
+			if(pstmt != null) {
+				pstmt.close();
+			}
+			
+			if(conn != null) {
+				conn.close();
+			}
+		} catch(Exception e) {
+			System.out.println("예외발생: " + e.getMessage());
+		}
+		
+		
+		return re;
+	}
+}
---
+++ oracleTest/day0511/vo/SurveyVO.java
@@ -0,0 +1,59 @@
+package com.sist.vo;
+
+public class SurveyVO {
+	private int no;
+	private int grade;
+	private String gender, interests, course, leave;
+	
+	public SurveyVO(int no, int grade, String gender, String interests, String course, String leave) {
+		super();
+		this.no = no;
+		this.grade = grade;
+		this.gender = gender;
+		this.interests = interests;
+		this.course = course;
+		this.leave = leave;
+	}
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getInterests() {
+		return interests;
+	}
+	public void setInterests(String interests) {
+		this.interests = interests;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public String getLeave() {
+		return leave;
+	}
+	public void setLeave(String leave) {
+		this.leave = leave;
+	}
+	public SurveyVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
